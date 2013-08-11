1c1a80a58be5d9589e87a7ecdb7d5308e4d3c01b
ihommani <ismaelhommani@gmail.com>
2013-08-11 14:05:47 +0200

    Added a property file for configuration. Read mow instruction from a file

--- src/main/java/mowitnow/Main.java
+++ src/main/java/mowitnow/Main.java
@@ -1,42 +1,51 @@
 package mowitnow;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
+
+import com.google.common.collect.Lists;
 
 import mowitnow.enums.CardinalPoint;
 import mowitnow.enums.Movement;
-
+/**
+ * Main routine from mowitnow package.
+ * Initiate a field and two mowers to mow the field 
+ * @author ihommani
+ *
+ */
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws FileNotFoundException, IOException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-
-	    Tondeuse tondeuse1 = (Tondeuse) context.getBean("tondeuse1");
-        Tondeuse tondeuse2 = (Tondeuse) context.getBean("tondeuse2");
-
-        tondeuse1.move(Movement.G);
-        tondeuse1.move(Movement.A);
-        tondeuse1.move(Movement.G);
-        tondeuse1.move(Movement.A);
-        tondeuse1.move(Movement.G);
-        tondeuse1.move(Movement.A);
-        tondeuse1.move(Movement.G);
-        tondeuse1.move(Movement.A);
-        tondeuse1.move(Movement.A);
-        
-        tondeuse2.move(Movement.A);
-        tondeuse2.move(Movement.A);
-        tondeuse2.move(Movement.D);
-        tondeuse2.move(Movement.A);
-        tondeuse2.move(Movement.A);
-        tondeuse2.move(Movement.D);
-        tondeuse2.move(Movement.A);
-        tondeuse2.move(Movement.D);
-        tondeuse2.move(Movement.D);
-        tondeuse2.move(Movement.A);
-
-        tondeuse1.finalPosition();
-        tondeuse2.finalPosition();
+		
+		Resource directivesFile = context.getResource("directives.txt");
+		
+		if(directivesFile.exists()){
+	        List<Tondeuse> tondeuses = Lists.newArrayList((Tondeuse) context.getBean("tondeuse1"), (Tondeuse) context.getBean("tondeuse2"));
+	        FileReader directives = new FileReader(directivesFile.getFile());
+	        BufferedReader br = new BufferedReader(directives);
+	        
+	        String line;
+	        int lastIndex =  0;
+	        while((line = br.readLine()) != null){
+	        	Tondeuse currentTondeuse = tondeuses.get(lastIndex);
+	        	for(char c: line.toCharArray()){
+	        		currentTondeuse.move(Movement.valueOf(Character.toString(c)));
+	        	}
+	        	currentTondeuse.finalPosition();
+	        	currentTondeuse = null; // Small pimp, for we do not need the mower anymore.
+	        	lastIndex++;
+	        }
+	        br.close();
+		}
     }
-
 }
